49b66a0029392169a0e81a4eb00469f957c89072
bloodstone <maks.khokholov.99@gmail.com>
2020-07-01 02:50:38 +0300

    uyt

---
+++ lesson3/task1/src/Search.jsx
@@ -0,0 +1,19 @@
+import './search.scss';
+
+const rootElement = document.querySelector('#root');
+
+ const Search = (props) => {
+     console.log(props);
+     return (
+        <div className="search">
+            <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input"/>
+                <button className="search__button">Search</button>
+            </div>
+        </div>
+    );
+};
+
+
+ReactDOM.render(<SearchField name='Bob' />, rootElement);
