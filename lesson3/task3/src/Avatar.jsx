0e7b9e77ca6ee492562c100529bbebd3435122ef
bloodstone <maks.khokholov.99@gmail.com>
2020-07-01 17:36:10 +0300

    hgf

---
+++ lesson3/task3/src/Avatar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Avatar = (props)=> {
+    return (
+        <img
+          className="avatar"
+          src={props.avatarUrl}
+          alt={props.name}
+        />
+    );
+};
+
+
+export default Avatar;
---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,18 @@
+  
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import './Comment.scss'
+import Comment from './Comment'
+
+const rootElement = document.querySelector('#root');
+
+const author = {
+    name: 'Tom',
+    avatarUrl: 'https://avatars1.githubusercontent.com/u10001'
+};
+
+
+ReactDOM.render(
+    <Comment author={author}text="Good job!"date={new Date('2019-01-01T11:32:19.566Z')} />,
+    rootElement);
