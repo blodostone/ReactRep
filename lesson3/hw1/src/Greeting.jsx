40629be4eeed8a74da3517ba7f12e4986a64f7c1
bloodstone <maks.khokholov.99@gmail.com>
2020-07-02 21:46:47 +0300

    lkj

---
+++ lesson3/hw1/src/Greeting.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const Greeting = (props) => {
+  return (
+    <div className='greeting'>
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${age(
+        props.birthDate
+      )} years old`}
+    </div>
+  );
+};
+
+export default Greeting;
