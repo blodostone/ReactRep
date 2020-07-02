e5b5530209c2ef4740212b3123e263923c9b5803
bloodstone <maks.khokholov.99@gmail.com>
2020-07-02 21:58:32 +0300

    hgf

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
+
 const Greeting = (props) => {
+    const age = (new Date() - new Date(props.birthDate));
   return (
     <div className='greeting'>
       {`My name is ${props.firstName} ${props.lastName}. I'm ${age(
