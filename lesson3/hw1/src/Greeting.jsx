988a2729a5305671eb16a6a44a823d4742292c55
bloodstone <maks.khokholov.99@gmail.com>
2020-07-03 14:04:55 +0300

    hfg

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 
 
+const birthDate = (date) => moment(date).format("DD MMM YY");
 
 const Greeting = (props) => {
-    const age = new Date(props.birthDate)
 
   return (
     <div className="greeting">
-      {`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${birthDate} years old`}
     </div>
   );
 };
