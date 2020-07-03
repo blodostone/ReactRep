dc10c010046e72dbfb34564854d0a6247d91b58b
bloodstone <maks.khokholov.99@gmail.com>
2020-07-03 03:11:49 +0300

    jhg

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 
 const Greeting = (props) => {
-    const age = new Date(props.birthDate).getDays().getFullYear()
+    const age = new Date(props.birthDate).getFullYear().getDays()
   return (
     <div className='greeting'>
       {`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}
