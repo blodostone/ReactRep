0fa56aa74f1e4a09932afa11632cd929effdcd04
bloodstone <maks.khokholov.99@gmail.com>
2020-07-03 14:29:55 +0300

    hfg

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -1,31 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import moment from "moment";
 
-
-const birthDate = (date) => moment(date).format("DD MMM YY");
-
-// const Greeting = (props) => {
-
-//   return (
-//     <div className="greeting">
-//       {`My name is ${props.firstName} ${props.lastName}. I'm ${birthDate(props.userData.birthDate} years old`}
-//     </div>
-//   );
-// };
-
-
-
-const birthDate = (date) => moment(date).format("DD MMM YY");
 
 const Greeting = (props) => {
-    return (
-      <>
-        <div className='greeting'>{`My name is ${props.firstName} ${props.lastName}. I'm ${birthDate(props.userData.birthDate)} years old`}</div>
-      </>
-    );
-  };
 
+  return (
+    <div className="greeting">
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${birthDate} years old`}
+    </div>
+  );
+};
 
 export default Greeting;
-
