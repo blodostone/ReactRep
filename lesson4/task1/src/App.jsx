8d71ae312b2a997fc6a399335f13c19e02a1d751
bloodstone <maks.khokholov.99@gmail.com>
2020-07-04 02:25:28 +0300

    jghn

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter.jsx'
+
+const App = ()=> {
+    return (
+        <>
+       <Counter start={17} interval={1000}/>
+       </>
+    );
+};
+
+
+export default App;
