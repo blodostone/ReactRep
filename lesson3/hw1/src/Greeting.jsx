55554e7081b996f6f2a28bba3a1775d7e8638b2c
bloodstone <maks.khokholov.99@gmail.com>
2020-07-03 16:00:27 +0300

    hgf

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
+const birthD = (birthDate) => {
+  ageInMiliSec = 31536000000;
+  return Math.floor((new Date() - new Date(birthDate)) / YearMs);
+};
+
+
 const Greeting = (props) => {
   
   return (
     <div className="greeting">
-      {`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${birthD} years old`}
     </div>
   );
 };
