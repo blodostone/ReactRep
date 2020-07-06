52ff3e5e2f559528b6393e7956283e18f0c7b691
bloodstone <maks.khokholov.99@gmail.com>
2020-07-06 16:05:54 +0300

    jhgn

--- lesson5/task1/src/index.jsx
+++ lesson5/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton';
+import GoodButton from './GoodButton.jsx';
 
 const rootElement = document.querySelector('#root');
 
