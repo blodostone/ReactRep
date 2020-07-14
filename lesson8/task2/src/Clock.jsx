62345481796ddd2531099a106abd64c926c3c340
bloodstone <maks.khokholov.99@gmail.com>
2020-07-14 13:18:44 +0300

    kjh

---
+++ lesson8/task2/src/Clock.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import getTimeWithOffset from "./getTimeWithOffset";
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: props.city,
+            offset: props.offset,
+            date: getTimeWithOffset(this.props.offset),
+        };
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState ({
+                date: getTimeWithOffset(this.props.offset),
+            })
+        }, 1000);
+    }
+
+    componentWillMount() {
+        clearInterval(this.interval);
+
+    }
+
+    render() {
+        return (
+            <div className="clock">
+                <div className="clock__location">
+                    {this.props.location}
+                </div>
+                <div className="clock__time">
+                    {this.state.date.toLocaleTimeString()}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Clock;
---
+++ lesson8/task2/src/getTimeWithOffset.jsx
@@ -0,0 +1,9 @@
+const getTimeWithOffset = (offset) => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(
+      currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+    );
+};
+  
+export default getTimeWithOffset;
