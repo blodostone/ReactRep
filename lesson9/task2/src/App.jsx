8e8f5037e658dccc69accfd9317401ab3033109c
bloodstone <maks.khokholov.99@gmail.com>
2020-07-16 11:01:20 +0300

    jh

---
+++ lesson9/task2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+class App extends React.Component {
+  createUser = (users) => {
+    console.log(users);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser}/>;
+  }
+}
+export default App;
