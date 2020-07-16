1d5d60436f4d55b27b9e952a5e2e38c6a899e04a
bloodstone <maks.khokholov.99@gmail.com>
2020-07-16 08:46:28 +0300

    jhg

---
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+//componentDidMount add status
+//
+//create offline status
+
+
+class ConnectionStatus extends React.Component {
+  state = {
+    status: 'online'
+  }
+
+
+  componentDidMount() {
+    window.addEventListener('online', this.setInternetOn);
+    window.addEventListener('offline', this.setInternetOff);
+  }
+
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.setInternetOn);
+    window.removeEventListener('offline', this.setInternetOff);
+  }
+
+
+  setInternetOn = () => {
+    this.setState({
+      status: 'online',
+    });
+  };
+
+
+  setInternetOff = () => {
+    this.setState({
+      status: 'offline',
+    });
+  };
+
+
+  render() {
+    if (this.state.status === 'offline') {
+      return <div className='status__offline'>{this.state.status}</div>;
+    }
+    return <div className='status'>{this.state.status}</div>;
+  }
+}
+
+export default ConnectionStatus;
+
