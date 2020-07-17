ddff4456d41d225b632a05c9efc256d32443969a
bloodstone <maks.khokholov.99@gmail.com>
2020-07-18 01:41:01 +0300

    uyt

---
+++ lesson9/task3/src/UserForm.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+class UserForm extends React.Component {
+
+  onSubmit = event => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(this.formRef));
+    this.props.onSubmit(formData);
+
+  };
+
+  setRef = node => {
+    this.formRef = node
+  }
+
+
+  render() {
+    return (
+      <form
+        ref={this.setRef}
+        className="login-form"
+        onSubmit={this.handleSumbit}
+      >
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-input" type="text" id="name" name="name" />
+        </div>
+        <div className="form-control">
+          <label className="form-label" htmlFor="student">
+            Student
+          </label>
+          <input
+            className="form-input"
+            type="checkbox"
+            id="student"
+            name="student"
+          />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="occupation" htmlFor="occupation">
+            Occupation
+          </label>
+          <select name="occupation" className="form-input">
+            <option value="London">London</option>
+            <option value="New-York">New York</option>
+            <option value="coconut">Sidney</option>
+            <option value="Berlin">Berlin</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="about" htmlFor="about">
+            About
+          </label>
+          <textarea name="about" className="form-input" />
+        </div>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default UserForm;
