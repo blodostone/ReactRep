383411ca08d16d4021267b9a0f246e418671c54f
bloodstone <maks.khokholov.99@gmail.com>
2020-07-20 17:35:55 +0300

    hgf

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import UserProfile from './UserProfile';
+import UserMenu from './UserMenu';
+
+class Page extends React.Component {
+    state = {
+        userData: null
+    };
+
+    componentDidMount() {
+        this.fetchUser(this.props.userId)
+    };
+
+    fetchUser = userId => {
+        fetch(`https://api.github.com/users/${userId}`)
+            .then(response => response.json())
+            .then(userData => {
+                this.setState({
+                    userData
+                });
+            });
+    };
+
+    render() {
+        return (
+            <div className="page">
+                <header className="header">
+                    <UserMenu userData={this.state.userData} />
+                </header>
+                <UserProfile userData={this.state.userData} />
+            </div>
+        )
+    }
+};
+
+export default Page;
