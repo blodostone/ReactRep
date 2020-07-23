a70310182d9286445a31aaf3ecf6d937c6e2f0a4
bloodstone <maks.khokholov.99@gmail.com>
2020-07-23 23:47:53 +0300

    hgfhf

---
+++ lesson11/task1/src/Numbers.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Numbers extends React.PureComponent {
+    render() {
+        return (   
+            <div className="number">
+                <span className="number__title">{this.props.title}</span>
+                <span className="number__value">{this.props.number}</span>
+            </div>           
+        );
+    };
+};
+
+
+export default Numbers;
