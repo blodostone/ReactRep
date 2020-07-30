15995cd7c327ca8d1eaa9816d0bb341c9edfa9f8
bloodstone <maks.khokholov.99@gmail.com>
2020-07-30 06:22:22 +0300

    hgf

---
+++ lesson12/task1/src/TasksList.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Task from './Task';
+import CreateTaskInput from './CreateTaskInput'
+import { createTask, fetchTasksList, updatedTask, deleteTask } from './tasksGateway'
+
+
+
+class TasksList extends React.Component {
+    state = {
+        tasks: [],
+    }
+
+    componentDidMount() {
+        this.fetchTasks()
+    }
+
+    fetchTasks = () => {
+        fetchTasksList()
+            .then(tasksList =>
+                this.setState({
+                    tasks: tasksList
+                }),
+            );
+    };
+
+
+    onCreate = text => {
+        const newTask = {
+            text,
+            done: false
+        }
+
+        createTask(newTask)
+        .then(() => this.fetchTasks())
+    }
+
+
+    handleTaskStatusChange = id => {
+        const { done, text } = this.state.tasks.find(task => task.id === id)
+        const upTask = {
+            ...text,
+            done: !done
+        };
+
+        updatedTask(id, upTask)
+            .then(() => this.fetchTasks())
+
+
+
+            
+    };
+
+    handleTaskDelete = id => {
+        deleteTask(id)
+        .then(() => this.fetchTasks())
+        
+    };
+
+
+    render() {
+        const sortedList = this.state.tasks
+            .slice()
+            .sort((a, b) => a.done - b.done)
+        
+        return (
+            <div className="todo-list">
+                <CreateTaskInput onCreate={this.onCreate} />
+                <ul className="list">
+                    {sortedList.map(task => (
+                        <Task
+                            key={task.id}
+                            {...task}
+                            onDelete={this.handleTaskDelete}
+                            onChange={this.handleTaskStatusChange}
+                        />
+                    ))}
+
+                </ul>
+            </div>
+        );
+    };
+};
+
+
+export default TasksList;
